1bf9a77fb3b80fd04f666440b45b6292d5ad284e
Jirasak-6510110071 <153747438+Jirasak-6510110071@users.noreply.github.com>
2023-12-17 18:23:21 +0700

    Update testlint.py

--- testlint.py
+++ testlint.py
@@ -1,15 +1,11 @@
-import numpy as np
-import time
-import pandas as pd
-
-Captain = "Picard"
+CAPTAIN = "Picard"
 
 
-def InitiateWarpSpeed(order):
+def initiateWarpSpeed(order):
     if order == "engage":
         print("initiating warp speed")
     else:
         print("you are not the captain of this vessel")
 
 
-InitiateWarpSpeed("engage")
+initiateWarpSpeed("engage")
