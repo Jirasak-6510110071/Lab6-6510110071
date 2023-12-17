8f04b3818156af36dd45d6f51bba4ab343a47a8f
Jirasak-6510110071 <153747438+Jirasak-6510110071@users.noreply.github.com>
2023-12-17 18:28:22 +0700

    Update testlint.py

--- testlint.py
+++ testlint.py
@@ -1,11 +1,11 @@
-captain = "Picard"
+CAPTAIN = "Picard"
 
 
 def initiate_warp_speed(order):
     if order == "engage":
-        print("initiating warp speed")
+        print("Initiating warp speed")
     else:
-        print("you are not the captain of this vessel")
-
+        print("You are not the captain of this vessel")
 
 initiate_warp_speed("engage")
+
