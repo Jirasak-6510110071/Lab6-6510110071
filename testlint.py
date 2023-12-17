ccf5b51f8c130a83266ce41357dfaeafcef3cc54
Jirasak-6510110071 <153747438+Jirasak-6510110071@users.noreply.github.com>
2023-12-17 18:31:16 +0700

    Update testlint.py

--- testlint.py
+++ testlint.py
@@ -1,20 +1,7 @@
-"""
-This module provides functions related to warp speed initiation.
-"""
-
 CAPTAIN = "Picard"
 
 
 def initiate_warp_speed(order):
-    """
-    Initiate warp speed based on the given order.
-
-    Parameters:
-    - order (str): The order given to initiate warp speed.
-
-    Returns:
-    None
-    """
     if order == "engage":
         print("Initiating warp speed")
     else:
@@ -22,4 +9,3 @@ def initiate_warp_speed(order):
 
 
 initiate_warp_speed("engage")
-
